d1b339c7da1a404828031ee48d8895fe6ed9ff3f
erdemir1 <erdemir@deloitte.com>
2025-05-31 12:11:11 +0300

    gitSCM changes

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,17 @@
 FROM jenkins/jenkins:lts
 
 USER root
-RUN apt-get update && apt-get install -y sudo git curl
+RUN apt-get update && apt-get install -y git
 
-USER jenkins
-
+# Pluginleri yükle
 COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
 RUN jenkins-plugin-cli --plugin-file /usr/share/jenkins/ref/plugins.txt
 
-COPY casc_configs /var/jenkins_home/casc_configs
-COPY init.groovy.d /var/jenkins_home/init.groovy.d
+# JCasC config ve groovy scriptlerini kopyala
+COPY casc_configs/ /var/jenkins_home/casc_configs/
+COPY init.groovy.d/ /var/jenkins_home/init.groovy.d/
+
 ENV CASC_JENKINS_CONFIG=/var/jenkins_home/casc_configs/jenkins.yaml
+ENV JAVA_OPTS="-Djenkins.install.runSetupWizard=false"
+
+USER jenkins
--- init.groovy.d/build-static-site.groovy
+++ init.groovy.d/build-static-site.groovy
@@ -1,9 +1,9 @@
 import jenkins.model.*
 import hudson.model.*
-import hudson.tasks.*
+import hudson.plugins.git.*
+import hudson.tasks.Shell
 
 def jenkins = Jenkins.getInstanceOrNull()
-
 if (jenkins != null) {
     def jobName = "build-static-site"
 
@@ -11,14 +11,19 @@ if (jenkins != null) {
         def job = jenkins.createProject(FreeStyleProject, jobName)
         job.setDescription("Builds and runs the static Docker site.")
 
+        def gitSCM = new GitSCM(
+            [new UserRemoteConfig("https://github.com/Erendmrkndmr/Erendmrkndmr.github.io", null, null, null)],
+            [new BranchSpec("*/dev")],
+            false, Collections.emptyList(), null, null, Collections.emptyList()
+        )
+        job.setScm(gitSCM)
+
         def shell = new Shell('''
-            git clone https://github.com/Erendmrkndmr/Erendmrkndmr.github.io || true
-            cd Erendmrkndmr.github.io
             docker stop static-site || true
             docker rm static-site || true
             docker rmi static-site || true
             docker build -t static-site .
-            docker run -d --name static-site -p 80:80 -v /var/run/docker.sock:/var/run/docker.sock static-site
+            docker run -d --name static-site -p 80:80 static-site
         ''')
 
         job.getBuildersList().add(shell)
