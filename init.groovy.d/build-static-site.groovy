e51ce3dc45bfdd0a52c39c08c7e7be2f66d96169
erdemir1 <erdemir@deloitte.com>
2025-05-31 12:05:38 +0300

    changes

--- init.groovy.d/build-static-site.groovy
+++ init.groovy.d/build-static-site.groovy
@@ -1,27 +1,27 @@
 import jenkins.model.*
 import hudson.model.*
+import hudson.tasks.*
 
-def jenkins = Jenkins.getInstance()
+def jenkins = Jenkins.getInstanceOrNull()
 
-def jobName = "build-static-site"
-if (jenkins.getItem(jobName) == null) {
-    def job = new FreeStyleProject(jenkins, jobName)
-    job.setDescription("Clones static site repo and builds Docker container")
+if (jenkins != null) {
+    def jobName = "build-static-site"
 
-    def gitScm = new hudson.plugins.git.GitSCM("https://github.com/Erendmrkndmr/Erendmrkndmr.github.io")
-    job.setScm(gitScm)
+    if (jenkins.getItem(jobName) == null) {
+        def job = jenkins.createProject(FreeStyleProject, jobName)
+        job.setDescription("Builds and runs the static Docker site.")
 
-    def shellStep = new hudson.tasks.Shell('''
-        docker stop static-site || true
-        docker rm static-site || true
-        docker rmi static-site || true
+        def shell = new Shell('''
+            git clone https://github.com/Erendmrkndmr/Erendmrkndmr.github.io || true
+            cd Erendmrkndmr.github.io
+            docker stop static-site || true
+            docker rm static-site || true
+            docker rmi static-site || true
+            docker build -t static-site .
+            docker run -d --name static-site -p 80:80 -v /var/run/docker.sock:/var/run/docker.sock static-site
+        ''')
 
-        docker build -t static-site .
-        docker run -d --name static-site -p 80:80 static-site
-    ''')
-
-    job.getBuildersList().add(shellStep)
-    jenkins.reload()
-    jenkins.add(job, jobName)
-    job.save()
+        job.getBuildersList().add(shell)
+        job.save()
+    }
 }
